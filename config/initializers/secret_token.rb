8e3fa5c61dd871ebced63f5ff9be659b45bf10fc
Nícolas Iensen <nicolas@quavio.com.br>
2011-11-28 00:09:42 -0200

    rails bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BloodRunner::Application.config.secret_token = 'd046174d7cd693acd7512d076ad863d694cce5bd0953996d8ecd45b250b047470e291776a0c370e65c9acb6de686102f57c0c0776a9cf5ae5409763798b03a15'
