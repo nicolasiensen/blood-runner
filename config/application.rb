947315a048a9abf9c2e7d3f708879322d4551557
Nícolas Iensen <nicolas@quavio.com.br>
2011-11-30 00:03:09 -0200

    precompile assets

--- config/application.rb
+++ config/application.rb
@@ -45,5 +45,6 @@ module BloodRunner
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.generators.stylesheet_engine = :sass
+    config.assets.precompile = %w(grid.css)
   end
 end
